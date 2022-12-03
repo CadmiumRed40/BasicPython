b86265dde6d9511d3fe6ebed655f9307611b2665
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-03 13:17:39 -0500

    python practice: modifying file to add a break statement

--- yourName.py
+++ yourName.py
@@ -1,5 +1,7 @@
 name = ''
-while name != 'your name':
+while True:
     print('Please type your name.')
     name = input()
+    if name == 'your name':
+        break #break statement causes the program to stop its while loop and skip to the next step.
 print('Thank you!')
