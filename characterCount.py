56f1f2cd3a43155c5ba3151bf6cf1fd4707fc0e8
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-04-22 16:36:36 -0400

    ..

---
+++ characterCount.py
@@ -0,0 +1,9 @@
+message = 'it was a bright cold day in april and, and the clocks were striking thirteen'
+count = {}
+
+for character in message.upper():
+    count.setdefault(character,0)
+    count[character] = count[character]+1
+
+print(count)
+    
