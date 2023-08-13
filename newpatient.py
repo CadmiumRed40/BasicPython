15c7c4314ba63e196e0f9a9688bc9b1a2ef0d0c6
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-08-13 16:31:27 -0400

    adjusted the program to practice other concepts.

--- newpatient.py
+++ newpatient.py
@@ -1,6 +1,9 @@
 name = input("Please enter your name? ")
-age = input("Hello, " + name + " What is your age? ")
+age = input("Hello, " + name + " enter your birth year: ")
+final_age = 2023 - int(age)
 new_patient = input("Are you a new patient? ")
 
 if new_patient == "yes":
     print("Welcome to PyHospital" + name + "!")
+else:
+    print("Sorry to see you here again. How can I help you today" + name +"?")
