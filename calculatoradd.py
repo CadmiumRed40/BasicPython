5ae28c2643f9e32628bff1a71d52e453cc0ff535
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-08-17 20:24:59 -0400

    converting strings into intergers and adding to get the sum

---
+++ calculatoradd.py
@@ -0,0 +1,4 @@
+first_number = input("Enter a number: " )
+second_number = input("Enter another number: " )
+sum = int(first_number) + int(second_number)
+print(sum)
--- countcharacter.py
+++ countcharacter.py
@@ -1,8 +1,10 @@
-message = "It was a a bright cold day in April and the clocks were striking thirteen."
-count = {} # "r" : 12
-for character in message:
+import pprint
+
+message = "It was a bright cold day in April and the clocks were striking thirteen."
+count = {} # "r" : 12 example of key result
+for character in message.upper():
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-print(count)
+pprint.pprint(count.sort())
 
