f6fc2b7068911e8655e31ae61737440989c2a1ba
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-08-23 23:52:47 -0400

    creating a dictionary

---
+++ listSearch.py
@@ -0,0 +1,21 @@
+students = [{
+    'name': 'Ben', 
+    'age': '13', 
+    'grade': '8', 
+    'email':'ben@school.com'
+    },
+    {
+        'name':'Alan',
+        'age': '12',
+        'grade': '7',
+        'email':'alan@school.com'
+    },
+    {
+        'name':'Carly', 
+        'age': '14', 
+        'grade': '9', 
+        'email':'carly@school.com'
+    }
+]
+
+
