ed2cbe69669b2566290740870a8824f7c637b254
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-08-24 00:13:54 -0400

    created a program to get student information to put into a dictionary

--- listSearch.py
+++ listSearch.py
@@ -1,21 +1,29 @@
-students = [{
-    'name': 'Ben', 
-    'age': '13', 
-    'grade': '8', 
-    'email':'ben@school.com'
-    },
-    {
-        'name':'Alan',
-        'age': '12',
-        'grade': '7',
-        'email':'alan@school.com'
-    },
-    {
-        'name':'Carly', 
-        'age': '14', 
-        'grade': '9', 
-        'email':'carly@school.com'
-    }
-]
+num_students = int(input("How many students will you be adding? "))
 
+students = []
+
+
+for _ in range(num_students):
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
+    grade = int(input("Enter grade: "))
+    email_input = input("Enter email: ")
+
+    student = {
+        'name': name,
+        'age': age,
+        'grade': grade,
+        'email': email_input
+    }
+
+students.append(student)
+
+print("Thank you for entering student infomation")
+
+for student in students:
+    print("Name:", student['name'])
+    print("Age:", student['age'])
+    print("Grade:", student['grade'])
+    print("Email:", student['email'])
+    print('-' * 30)
 
