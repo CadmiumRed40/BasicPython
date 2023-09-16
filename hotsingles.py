bd7a7188752f0ddb153e54fa3a77e28315cbbf33
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-09-15 22:37:30 -0400

    added another solution

--- hotsingles.py
+++ hotsingles.py
@@ -9,3 +9,12 @@ def hot_singles(arr1, arr2):
 #The main takaway here is to take the arguments and converts arrays to remove 
 # duplicates and then using the built in function "symmetric difference", you 
 # can pull the difference between arrays and set them as the result variable.
+
+#Another solution:
+
+def hot_singles(arr1, arr2):
+    a = []
+    for x in arr1 + arr2:
+        if x in set(arr1) ^ set(arr2) and x not in a: a.append(x)
+    return a
+
